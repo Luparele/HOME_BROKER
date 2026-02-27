f89b9b3e8cef6c79f521ed3bf07e8368b1e92213
Luparele <eduardo.luparele@gmail.com>
2026-02-27 14:20:56 -0300

    Admin: register all models dynamically

--- stocks/admin.py
+++ stocks/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.apps import apps
+from .models import Favorite, PortfolioItem
 
-# Register your models here.
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'ticker', 'name', 'added_at')
+    search_fields = ('user__username', 'ticker', 'name')
+    list_filter = ('added_at',)
+
+@admin.register(PortfolioItem)
+class PortfolioItemAdmin(admin.ModelAdmin):
+    list_display = ('user', 'ticker', 'name', 'quantity', 'added_at')
+    search_fields = ('user__username', 'ticker', 'name')
+    list_filter = ('added_at', 'updated_at')
+
+# Automatiza o registro de todas as tabelas (models) no admin
+models = apps.get_models()
+for model in models:
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
